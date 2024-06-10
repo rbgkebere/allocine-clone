b8cbf2b293a4f6f5a206a00b48608eaa0421ccd2
rbgkebere <bkebere10@gmail.com>
2024-06-10 18:50:18 +0000

    fetch data from tmdb api

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,28 @@
+import Results from '@/components/Results'
 import React from 'react'
 
-export default function page() {
+const API_KEY = process.env.API_KEY
+
+export default async function page({searchParams}) {
+ 
+  const genre = searchParams.genre || 'fetchTrending'
+  const res = await fetch (
+    `https://api.themoviedb.org/3${genre==='fetchTopRated'?
+      `/movie/top_rated`:
+      `/trending/all/week`}?api_key=${API_KEY}&langiage=en-US&page=1`
+  )
+
+  const data = await res.json()
+
+  if (!res.ok){throw new Error('Failed to fetch data')}
+
+  const results = data.results
+
+  console.log(results)
+
   return (
-    <div className='text-red-500'>
+    <div>
+      <Results results={results}/>
     </div>
   )
 }
