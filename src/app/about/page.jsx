b79b00ba049659eb6a7ed0301b7a540121d2233a
rbgkebere <bkebere10@gmail.com>
2024-06-10 17:07:50 +0000

    created the about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function About() {
+    const b ='Hello'
+const a =<p>{b}</p>
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4 justify-normal'>
+        <h1 className='text-2xl font-medium
+        text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, excepturi! Omnis, officiis laborum magnam commodi, iure perferendis molestiae tenetur veniam, voluptatem totam repellendus odio corrupti sequi officia quibusdam temporibus ducimus.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, excepturi! Omnis, officiis laborum magnam commodi, iure perferendis molestiae tenetur veniam, voluptatem totam repellendus odio corrupti sequi officia quibusdam temporibus ducimus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea itaque totam odio quod cupiditate quo labore quam! Aperiam, modi maiores ratione perspiciatis tempore alias aliquid eum est voluptates. Illo, quasi?
+    </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, excepturi! Omnis, officiis laborum magnam commodi, iure perferendis molestiae tenetur veniam, voluptatem totam repellendus odio corrupti sequi officia quibusdam temporibus ducimus.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odio mollitia expedita vero, excepturi dolor quidem voluptate omnis voluptates consequatur similique minima ut nemo quaerat quasi? Ab inventore amet hic. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque amet reiciendis sunt officiis vitae inventore, impedit sapiente culpa expedita nemo, in non excepturi magnam! Velit animi fugit non praesentium culpa.
+    </p>
+  
+    </div>
+    )
+}
