3739faed2f81a535f706176a150366bb56d41756
rbgkebere <bkebere10@gmail.com>
2024-06-12 16:57:23 +0000

    add of search box

--- src/app/movie/[id]/page.jsx
+++ src/app/movie/[id]/page.jsx
@@ -17,6 +17,12 @@ export default async function MoviePage({params}) {
         movie.backdrop_path || movie.poster_path
       }`}
       width={500} height = {300} className='rounded-lg'></Image>
+      <div className=''>
+        <h2 className='text-lg mb-3 font-bold'>{movie.title|| movie.name}</h2>
+        <p className='text-lg mb-3'>{movie.overview}</p>
+        <p className='mb-3'> <span className='font-semibold mr-1'>Date released: </span> {movie.release_date}</p>
+        <p> <span className='font-semibold mr-1'>Rating:</span> {movie.vote_count}</p>
+      </div>
       </div>
     </div>
   )
